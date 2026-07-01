d4f34c1d1989f58718fbb9574865c01f2a6b9b29
UJJAWAL KUMAR <ujjawal8032kumar@gmail.com>
2026-07-02 00:52:07 +0530

    Time: 1 ms (100.00%) | Memory: 49.1 MB (15.54%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,27 +1,30 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int i=0,j=0,k=0;
-        int[] nums = new int[nums1.length+nums2.length];
-        while(i<nums1.length && j<nums2.length){
-            if(nums1[i] <= nums2[j]){
-                nums[k++] = nums1[i++];
-            }else nums[k++] = nums2[j++];
-        }
-        while(i<nums1.length) nums[k++] = nums1[i++];
-        while(j<nums2.length) nums[k++] = nums2[j++];
-
-        int n = nums.length;
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        if(n1>n2) return findMedianSortedArrays(nums2,nums1);
         int lo = 0;
-        int hi = nums.length-1;
-        double median;
-        if(n%2==0){
+        int hi = n1;
+        int left = (n1+n2+1)/2;
+        int n = n1+n2;
+        while(lo <= hi){
             int mid1 = lo + (hi-lo)/2;
-            int mid2 = mid1+1;
-            median = ((double)nums[mid1] + nums[mid2]) / 2;
-        }else{
-            int mid = lo + (hi-lo)/2;
-            median = nums[mid];
+            int mid2 = left - mid1;
+            int l1 = Integer.MIN_VALUE;
+            int l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE;
+            int r2 = Integer.MAX_VALUE;
+            if(mid1 < n1) r1 = nums1[mid1];
+            if(mid2 < n2) r2 = nums2[mid2];
+            if(mid1-1 >=0) l1 = nums1[mid1-1];
+            if(mid2-1 >=0) l2 = nums2[mid2-1];
+            if(l1 <= r2 && l2 <= r1){
+                if(n%2==1) return Math.max(l1,l2);
+                else return ((double)Math.max(l1,l2)+Math.min(r1,r2))/2;
+            } 
+            if(l1 > r2) hi = mid1-1;
+            else lo = mid1+1;
         }
-        return median;
+        return 0;
     }
 }
